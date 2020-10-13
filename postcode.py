74a769e759e6d7b78e1233911c567e49eef46d5d
tmh <291676553@qq.com>
2020-10-13 17:00:36 +0800

    finish states'name,countries and its urls.also newing empty file

--- postcode.py
+++ postcode.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import os
+import numpy as np
 
 def getHTMl(url):
     headers = {
@@ -15,11 +17,37 @@ def write_to_file(content):
     with open('D:\TMH\g_postcode\Homepage.txt','a',encoding='UTF-8') as f:
         f.write(content)
 
-def parse_homepage(html):
-    pattern = re.compile('<a.*?dropdown.*?href="#">(.*?)<span.*?caret',re.S)
-    postcodes = re.findall(pattern,html)
-    for pc in postcodes:
-        print(pc)
+
+def mkdir(path):
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        print(path+'succ in making new path')
+        return True
+    else:
+        return False
+
+def parse_state(html):
+    pattern = re.compile('<a.*?dropdown.*?href="#">(.*?)<span.*?caret(.*?)</ul>',re.S)#state's name&whole part of the countries in it.
+    states = re.findall(pattern,html)
+    states_n = np.array(states)[0:5]#translate to narray
+    # states_n =states_n[0:5,:]#keep the important info
+    for s in states_n:
+        file_path_state='D:\TMH\g_postcode\\'+ s[0]
+        print(file_path_state)
+        # print(s[0])
+    pattern1 = re.compile('<li><a.*?href="(.*?)">(.*?)</a></li>',re.S)#countries and its urls.
+    states_s =str(states)#to string
+    countries = re.findall(pattern1,states_s)
+    countries_n = np.array(countries)[:-9]
+    # countries_n = np.delete(countries_n,[:-8],axis=0)#?
+    for c in countries_n:
+        print(c)
+    # for s in states:
+    #     yeild{
+    #         'state_name':s[0],
+    #         'country_url':s[1]
+    #     }
 
 
 if __name__ == "__main__":
@@ -27,4 +55,10 @@ if __name__ == "__main__":
     html = getHTMl(url)
     # print(html)
     write_to_file(html)
-    parse_homepage(html)
+    parse_state(html)
+
+
+
+    # state's name :<a.*?dropdown.*?href="#">(.*?)<span.*?caret
+    # countries :<li><a.*?href="(.*?)">(.*?)</a></li>
+    # state's name & the info of countries :<a.*?dropdown.*?href="#">(.*?)<span.*?caret(.*?)</ul>
